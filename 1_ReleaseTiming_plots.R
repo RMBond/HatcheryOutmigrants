fbf6ac4b4959c716ef23ee16f33f21f83033bde7
RMBond <lea.bond@gmail.com>
2020-03-10 15:43:02 -0700

    custom 1/2 violin plot

--- 1_ReleaseTiming_plots.R
+++ 1_ReleaseTiming_plots.R
@@ -160,5 +160,108 @@ ggplot(fish, aes(x = Ordinal_Day_From_Release, y = Days_to_Detect, fill = Ordina
   theme(strip.background = element_blank(), strip.text.x = element_blank()) +
   theme(legend.position =  "none", plot.title = element_text(size = 11)) 
 
+#Violin Boxplot by year
+ggplot(fish, aes(x = Ordinal_Day_From_Release, y = Days_to_Detect, fill = Water_Year)) +
+  geom_violin() +
+  stat_boxplot_custom(qs = c(0.1, 0.25, 0.5, 0.75, 0.9), outlier.shape = NA, width = 0.2, color = "grey", alpha = 0.2) +
+  facet_wrap(Water_Year ~ ., ncol = 1) +
+  # geom_point(data = Fish.Growth.ALL.SH.summary,aes(x = SamplingMonth.y, y = MSGR_mean), size = 2) + #add mean point for each month
+  theme_classic() +
+  scale_fill_viridis() +
+  scale_y_continuous(limits = c(0,60), breaks = custom_breaks,
+                     labels = every_nth(custom_breaks, 2, inverse = TRUE)) +
+  ylab("Days to Detection") +
+  xlab("Release Day") +
+  theme(strip.background = element_blank(), strip.text.x = element_blank()) +
+  theme(legend.position =  "none", plot.title = element_text(size = 11)) 
+
+
+####Disribution next to boxplot
+
+"%||%" <- function(a, b) {
+  if (!is.null(a)) a else b
+}
+
+geom_flat_violin <- function(mapping = NULL, data = NULL, stat = "ydensity",
+                             position = "dodge", trim = TRUE, scale = "area",
+                             show.legend = NA, inherit.aes = TRUE, ...) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomFlatViolin,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      trim = trim,
+      scale = scale,
+      ...
+    )
+  )
+}
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomFlatViolin <-
+  ggproto("GeomFlatViolin", Geom,
+          setup_data = function(data, params) {
+            data$width <- data$width %||%
+              params$width %||% (resolution(data$x, FALSE) * 0.9)
+            
+            # ymin, ymax, xmin, and xmax define the bounding rectangle for each group
+            data %>%
+              group_by(group) %>%
+              mutate(ymin = min(y),
+                     ymax = max(y),
+                     xmin = x,
+                     xmax = x + width / 2)
+            
+          },
+          
+          draw_group = function(data, panel_scales, coord) {
+            # Find the points for the line to go all the way around
+            data <- transform(data, xminv = x,
+                              xmaxv = x + violinwidth * (xmax - x))
+            
+            # Make sure it's sorted properly to draw the outline
+            newdata <- rbind(plyr::arrange(transform(data, x = xminv), y),
+                             plyr::arrange(transform(data, x = xmaxv), -y))
+            
+            # Close the polygon: set first and last point the same
+            # Needed for coord_polar and such
+            newdata <- rbind(newdata, newdata[1,])
+            
+            ggplot2:::ggname("geom_flat_violin", GeomPolygon$draw_panel(newdata, panel_scales, coord))
+          },
+          
+          draw_key = draw_key_polygon,
+          
+          default_aes = aes(weight = 1, colour = "grey20", fill = "white", size = 0.5,
+                            alpha = NA, linetype = "solid"),
+          
+          required_aes = c("x", "y")
+  )
+
+
+# Final plot inspired from @jbburant: https://gist.github.com/jbburant/b3bd4961f3f5b03aeb542ed33a8fe062
+
+ggplot(fish, aes(x = Ordinal_Day_From_Release, y = Days_to_Detect, fill = Ordinal_Day_From_Release, group = Ordinal_Day_From_Release)) + 
+  facet_wrap(Water_Year ~ ., ncol = 1) +
+  geom_flat_violin(scale = "count", trim = FALSE, width = 10, position = position_nudge(.3)) +
+  scale_fill_viridis() +
+  stat_boxplot_custom(qs = c(0.1, 0.25, 0.5, 0.75, 0.9), outlier.shape = NA, width = 0.5, color = "black", alpha = 0.25) +
+  # stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "pointrange") +
+  # geom_dotplot(binaxis = "y", dotsize = 0.8, stackdir = "down", binwidth = 0.3, position = position_nudge(-0.025)) + 
+  theme_classic() +
+  theme(legend.position = "none") + 
+  scale_y_continuous(limits = c(0,100), breaks = custom_breaks,
+                     labels = every_nth(custom_breaks, 2, inverse = TRUE)) +
+  ylab("Days to Detection") +
+  xlab("Release Day") +
+  theme(strip.background = element_blank(), strip.text.x = element_blank())
+
 
 
